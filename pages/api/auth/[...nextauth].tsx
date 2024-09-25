0111b6aa68a508a48fa4b10500b43455aabd7b24
Daniel Henrique <ddanielhpf@gmail.com>
2024-09-24 21:54:35 -0300

    Update [...nextauth].tsx

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
-import { FirebaseAdapter } from '@next-auth/firebase-adapter'
+import { FirestoreAdapter } from '@next-auth/firebase-adapter'
 import { db } from '../../../src/firebase'
 import * as firestoreFunctions from 'firebase/firestore'
 
